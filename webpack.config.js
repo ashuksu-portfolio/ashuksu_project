c28107e87cc090d23a5dd40c3e4a6dcd8e68680f
liutyi@webspark.com <liutyi@webspark.com>
2022-05-07 22:14:47 +0300

    dev - created project

---
+++ webpack.config.js
@@ -0,0 +1,138 @@
+const path = require("path");
+const fs = require("fs");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+function generateHtmlPlugins(templateDir) {
+  const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
+  return templateFiles.map((item) => {
+    const parts = item.split(".");
+    const name = parts[0];
+    const extension = parts[1];
+    return new HtmlWebpackPlugin({
+      filename: `${name}.html`,
+      template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
+      inject: 'body',
+      scriptLoading: 'blocking',
+    });
+  });
+}
+
+const htmlPlugins = generateHtmlPlugins("src/html/views");
+
+const config = {
+  entry: ["./src/js/index.js", "./src/scss/style.scss"],
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "js/bundle.[contenthash].js",
+    clean: true,
+  },
+  devtool: "source-map",
+  mode: "production",
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin({
+        minimizerOptions: {
+          preset: [
+            "default",
+            {
+              discardComments: { removeAll: true },
+            },
+          ],
+        },
+      }),
+      new TerserPlugin({
+        extractComments: true,
+      }),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sass|scss)$/,
+        include: [
+            path.resolve(__dirname, "src/scss"),
+            path.resolve(__dirname, '../node_modules'),
+          ],
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {},
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+              url: false,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: [
+                require('autoprefixer')({
+                  browsers: ['> 1%', 'last 2 versions'],
+                }),
+              ],
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        include: path.resolve(__dirname, "src/html/includes"),
+        use: ["raw-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/style.bundle.[contenthash].css",
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "src/fonts",
+          to: "fonts",
+        },
+        {
+          from: "src/favicon",
+          to: "favicon",
+        },
+        {
+          from: "src/img",
+          to: "img",
+        },
+        {
+          from: "src/uploads",
+          to: "uploads",
+        },
+      ],
+    }),
+  ].concat(htmlPlugins),
+};
+
+module.exports = (env, argv) => {
+  if (argv.mode === "production") {
+    config.plugins.push(new CleanWebpackPlugin());
+  }
+  return config;
+};
